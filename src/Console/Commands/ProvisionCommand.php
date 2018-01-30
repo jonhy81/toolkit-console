eb90362264042a2d197e554227a4fe0c9163b690
Joao Santos <jonhy81@gmail.com>
2018-01-30 19:07:52 +0100

    Initial commit.

--- src/Console/Commands/ProvisionCommand.php
+++ src/Console/Commands/ProvisionCommand.php
@@ -1,6 +1,6 @@
 <?php
 // src/Command/CreateUserCommand.php
-namespace App\Command;
+namespace Toolkit\Console\Commands;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,6 +17,18 @@ class ProvisionCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+      // outputs multiple lines to the console (adding "\n" at the end of each line)
+      $output->writeln([
+        'User Creator',
+        '============',
+        '',
+      ]);
+
+      // outputs a message followed by a "\n"
+      $output->writeln('Whoa!');
+
+      // outputs a message without adding a "\n" at the end of the line
+      $output->write('You are about to ');
+      $output->write('create a user.');
     }
 }
